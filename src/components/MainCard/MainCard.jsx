f54a26d0ccddf49e29d9f978da148acd4627bda8
Ruan <rugcosta1234@gmail.com>
2024-07-08 20:09:15 -0300

    Add: finalizando o componente do perfil

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,25 +1,31 @@
-import styles from './MainCard.module.css'
+import styles from "./MainCard.module.css";
 export default function MainCard() {
   return (
     <section>
-      <div className={styles.mainCardContainer}>
-        <picture>
-          <img src="time_tracking_dashboard/image-jeremy.png" alt="image-jeremy" />
-        </picture>
-        <p>Report for</p>
-        <h1>Jeremy Robson</h1>
+      <div className={styles.maiProfileContainer}>
+        <div className={styles.subProfileContainer}>
+          <picture className={styles.imgContainer}>
+            <img
+              className={styles.imgProfile}
+              src="time_tracking_dashboard/image-jeremy.png"
+              alt="image-jeremy"
+            />
+          </picture>
+          <p className={styles.textProfile}>Report for</p>
+          <h1 className={styles.titleProfile}>Jeremy Robson</h1>
+        </div>
+        <ul className={styles.buttonProfile}>
+          <li>
+            <a href="">Daily</a>
+          </li>
+          <li>
+            <a href="">Weekly</a>
+          </li>
+          <li>
+            <a href="">Monthly</a>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <button>Daily</button>
-        </li>
-        <li>
-          <button>Weekly</button>
-        </li>
-        <li>
-          <button>Monthly</button>
-        </li>
-      </ul>
     </section>
   );
 }
